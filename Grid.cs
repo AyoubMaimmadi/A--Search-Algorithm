9808398489b65f37360df98ab09ee40fbdcfd6fb
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-07 16:36:54 +0100

    new

--- Grid.cs
+++ Grid.cs
@@ -19,6 +19,19 @@ public class Grid : MonoBehaviour
 		CreateGrid();
 	}
 
+    void CreateGrid() {
+		grid = new Node[gridSizeX,gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+
+		for (int x = 0; x < gridSizeX; x ++) {
+			for (int y = 0; y < gridSizeY; y ++) {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+				grid[x,y] = new Node(walkable,worldPoint);
+			}
+		}
+	}
+
 
     void OnDrawGizmos() {
 		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
