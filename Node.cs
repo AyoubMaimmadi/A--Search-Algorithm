748765a6e65dcf22a7530eeb0cd8d2efbd7b9e77
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-07 14:32:09 +0100

    new

--- Node.cs
+++ Node.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
-{
-  
+public class Node {
+    
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool _walkable, Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+    }
+
 }
