768b2ee40c83bf3a9fe3464e309a495c21afd0f0
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-07 14:34:02 +0100

    new

--- Grid.cs
+++ Grid.cs
@@ -8,4 +8,14 @@ public class Grid : MonoBehaviour
 	public Vector2 gridWorldSize;
 	public float nodeRadius;
 	Node[,] grid;
+
+    float nodeDiameter;
+	int gridSizeX, gridSizeY;
+
+	void Start() {
+		nodeDiameter = nodeRadius*2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+		CreateGrid();
+	}
 }
