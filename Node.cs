377d7a2f774bc64891c439cb8c60e4cb5297dcf6
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-07 17:07:19 +0100

    new

--- Node.cs
+++ Node.cs
@@ -1,22 +1,26 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Node
-{
-    public bool walkable;
-    public Vector3 worldPosition;
+public class Node {
+	
+	public bool walkable;
+	public Vector3 worldPosition;
+	public int gridX;
+	public int gridY;
 
-    public int gCost;
-    public int hCost;
+	public int gCost;
+	public int hCost;
+	public Node parent;
+	
+	public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
+		walkable = _walkable;
+		worldPosition = _worldPos;
+		gridX = _gridX;
+		gridY = _gridY;
+	}
 
-    public Node(bool _walkable, Vector3 _worldPosition)
-    {
-        walkable = _walkable;
-        worldPosition = _worldPosition;
-    }
-
-    public int fCost {
+	public int fCost {
 		get {
 			return gCost + hCost;
 		}
