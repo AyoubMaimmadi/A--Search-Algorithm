5fa422d6252a40c4ba117d127876faead7329bfc
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-07 14:34:50 +0100

    new

--- Grid.cs
+++ Grid.cs
@@ -31,4 +31,15 @@ public class Grid : MonoBehaviour
 			}
 		}
 	}
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition) {
+		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
+		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
+		return grid[x,y];
+	}
 }
