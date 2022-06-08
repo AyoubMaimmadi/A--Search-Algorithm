41faff67c9e8fb3982056c1c1cfbc3d3b32a19d1
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-08 16:34:16 +0100

    new

--- Node.cs
+++ Node.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Node {
-	
+	// walkable or not (if not, it's a wall)
 	public bool walkable;
+	// what point in the world this node represents
 	public Vector3 worldPosition;
 	public int gridX;
 	public int gridY;
@@ -13,6 +14,7 @@ public class Node {
 	public int hCost;
 	public Node parent;
 	
+	// constructor for the node class 
 	public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
 		walkable = _walkable;
 		worldPosition = _worldPos;
