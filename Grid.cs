ce5b1dfbddd24efd39342a0c0a3a14a3f4d2f55d
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-09 13:48:31 +0100

    new

--- Grid.cs
+++ Grid.cs
@@ -28,8 +28,8 @@ public class Grid : MonoBehaviour {
 	void CreateGrid() {
 		grid = new Node[gridSizeX,gridSizeY];
 
-		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
 		// check if the node is walkable or not (collision check) 
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
 		for (int x = 0; x < gridSizeX; x ++) {
 			for (int y = 0; y < gridSizeY; y ++) {
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
